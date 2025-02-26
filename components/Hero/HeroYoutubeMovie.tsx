aa56690beeb8d0ca8efe941035415adea31b8127
nicknijenhuis <n.nijenhuis@action.nl>
2025-02-26 16:25:23 +0100

    use iframe instead of youtube embed for aftermovie

--- components/Hero/HeroYoutubeMovie.tsx
+++ components/Hero/HeroYoutubeMovie.tsx
@@ -1,29 +1,17 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-import { YouTubeProps } from 'react-youtube';
-
-const YouTube = dynamic(() => import('react-youtube') as unknown as Promise<React.ComponentType<YouTubeProps>>, { ssr: false });
-
 const HeroYoutubeMovie = () => {
-    const opts: YouTubeProps['opts'] = {
-        width: '100%',
-        height: '100%',
-        playerVars: {
-            autoplay: 0,
-        },
-    };
-
     return (
         <div className="h-full w-full flex justify-center items-center group
             rounded-xl min-w-[35vw] shadow-lg md:ml-5 bg-black">
 
             <div className="relative w-full h-full">
-                <YouTube
-                    videoId="81-IQsNImXU"
-                    opts={opts}
-                    className="absolute py-4 top-0 left-0 w-full h-full object-cover"
-                />
+
+                <iframe className="absolute top-0 left-0 w-full h-full py-3" src="https://www.youtube.com/embed/81-IQsNImXU?si=XPwgI3fm_8qHyHzp" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+                    
+                </iframe>
+
             </div>
         </div>
     );
