dd48391f403b78838391875fb42ca5f9956031b5
nicknijenhuis <n.nijenhuis@action.nl>
2025-02-26 19:44:01 +0100

    and now this iframe also on mobile

--- components/Hero/HeroYoutubeMovie.tsx
+++ components/Hero/HeroYoutubeMovie.tsx
@@ -2,12 +2,12 @@
 
 const HeroYoutubeMovie = () => {
     return (
-        <div className="h-full w-full flex justify-center items-center group
+        <div className="flex flex-col h-full w-full justify-center items-center group
             rounded-xl min-w-[35vw] shadow-lg md:ml-5 bg-black">
 
             <div className="relative w-full h-full">
 
-                <iframe className="absolute top-0 left-0 w-full h-full py-3" src="https://www.youtube.com/embed/81-IQsNImXU?si=XPwgI3fm_8qHyHzp" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                <iframe className="w-full h-full py-3" src="https://www.youtube.com/embed/81-IQsNImXU?si=XPwgI3fm_8qHyHzp" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
                     
                 </iframe>
